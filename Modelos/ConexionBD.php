5729c71164723a28494304e09900096777c7aa53
angello2020 <azamudio@uoc.edu>
2020-05-25 08:54:50 +0200

    Conexión bbdd

--- Modelos/ConexionBD.php
+++ Modelos/ConexionBD.php
@@ -9,13 +9,13 @@
 
             try{
                 $this->conexionBD = new PDO('mysql:host=localhost; dbname=cc_glories', 'root', '');
-                //$this->conexionBD = new PDO('mysql:host=localhost; dbname=epiz_25852538_glories', 'epiz_25852538', 'M3OCJofK14CZGu');
+                //$this->conexionBD = new PDO('mysql:host=sql106.epizy.com;dbname=epiz_25852538_glories','epiz_25852538','M3OCJofK14CZGu');
                 $this->conexionBD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $this->conexionBD->exec("SET CHARACTER SET utf8");
                 return $this->conexionBD;
             }catch(Exception $exepcio) {
                 echo "<<<< ATENCIÓN >>>>  - ERROR: No se ha podido conectar con la Base de datos porque:<br><br>";
-               // echo $exepcio->getMessage();
+                echo $exepcio->getMessage();
 
 
             }
